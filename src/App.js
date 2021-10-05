52f784045122bd36a7e4f091153a03b0c6292ff6
hasina <0000hasman@gmail.com>
2021-10-05 19:33:10 +0300

    vueb

--- src/App.js
+++ src/App.js
@@ -1,75 +1,54 @@
-import './components/vueA.js'
+import './components/vueA.js';
+import './components/vueb.js';
+import './components/vueb.css'
 import './App.css';
 
 function App1 () {
 
 
     return(
-      <div className = "vue_container">
-        <section className="home"> 
-          <div className = "menu">
+     <div>
+        <div className="vueb_container">
+        <section>
+          <div className="menu">
+          <i class="fa fa-home " ></i>
             
           </div>
-          <div className="type">
-            <button className="btn">+ Nouvelle partie</button>
-            <div className="list-name">
-            <div className="nom-de-joueur">
-                <p className="joueur"> T.N.P (EN COURS)</p>
-                <h6 className="dureé">il y a 1heure</h6>
-                    <div className="des-joueur">
-                      <h5 className="name">tojo:75</h5>
-                      <h5 className="name">papi:125</h5>
-                      <h5 className="name">nant:51</h5>
-                    </div>
-           
+          <div className="titre">
+            <h1 className="h1">Nouvelle partie</h1>
+            <div className="type_de_joueur">
+
+            <div className="input">
+              <h6 className="margin" >joueur1</h6>
+              <input src="text" className="test" placeholder="nom du joueur1"></input>
 
             </div>
-            <div className="nom-de-joueur">
-                <p className="joueur"> K.M</p>
-                <h6 className="dureé">il y a 1heure</h6>
-                    <div className="des-joueur">
-                      <h5 className="name">koto:62</h5>
-                      
-                      <h5 className="name">mahery:130</h5>
-                    </div>
-           
+            <div className="input">
+              <h6 className="margin">joueur2</h6>
+              <input src="text" className="test" placeholder="nom du joueur2"></input>
 
             </div>
-            <div className="nom-de-joueur">
-                <p className="joueur"> T.N.P</p>
-                <h6 className="dureé">il y a 1heure</h6>
-                    <div className="des-joueur">
-                      <h5 className="name">tojo:75</h5>
-                      <h5 className="name">papi:125</h5>
-                      <h5 className="name">nant:14</h5>
-                    </div>
-           
+            <div className="input">
+              <h6 className="margin">joueur3</h6>
+              <input src="text" className="test" placeholder="nom du joueur3"></input>
 
             </div>
-            <div className="nom-de-joueur">
-                <p className="joueur"> T.N.P </p>
-                <h6 className="dureé">il y a 1heure</h6>
-                    <div className="des-joueur">
-                      <h5 className="name">tojo:75</h5>
-                      <h5 className="name">papi:125</h5>
-                      <h5 className="name">nant:49</h5>
-                    </div>
-           
+            <div className="input">
+              <h6 className="margin">Score gagnant</h6>
+              <input src="text" className="test" placeholder="score gagnant"></input>
 
             </div>
-            </div>
-           
-           
-            
-            
 
+
+            </div>
+            <div className="click">
+              <button className="annuler">  <i class="fa fa-chevron-left"> </i> Annuler</button>
+              <button className="red"> Ajouter <i class="fa fa-plus"></i></button>
+            </div>
           </div>
-          
-
         </section>
-          
-          
       </div>
+     </div>
     )
  
 }
--- src/components/vueA.js
+++ src/components/vueA.js
@@ -1,9 +1,74 @@
 import './vue.css';
-  export  const vueA =()=>{
+  export  const vueA =(props)=>{
     return(
          <div >
-             
+             <div className = "vue_container">
+        <section className="home"> 
+          <div className = "menu">
+         
+            
+          </div>
+          <div className="type">
+            <button className="btn">+ Nouvelle partie</button>
+            <div className="list-name">
+            <div className="nom-de-joueur">
+                <p className="joueur"> T.N.P (EN COURS)</p>
+                
+                <h6 className="dureé">il y a 1heure</h6>
+                    <div className="des-joueur">
+                      <h5 className="name">tojo:75</h5>
+                      <h5 className="name">papi:125</h5>
+                      <h5 className="name">nant:51</h5>
+                    </div>
+           
+
+            </div>
+            <div className="nom-de-joueur">
+                <p className="joueur"> K.M</p>
+                <h6 className="dureé">il y a 1heure</h6>
+                    <div className="des-joueur">
+                      <h5 className="name">koto:62</h5>
+                      
+                      <h5 className="name">mahery:130</h5>
+                    </div>
+           
+
+            </div>
+            <div className="nom-de-joueur">
+                <p className="joueur"> T.N.P</p>
+                <h6 className="dureé">il y a 1heure</h6>
+                    <div className="des-joueur">
+                      <h5 className="name">tojo:75</h5>
+                      <h5 className="name">papi:125</h5>
+                      <h5 className="name">nant:14</h5>
+                    </div>
+           
+
+            </div>
+            <div className="nom-de-joueur">
+                <p className="joueur"> T.N.P </p>
+                <h6 className="dureé">il y a 1heure</h6>
+                    <div className="des-joueur">
+                      <h5 className="name">tojo:75</h5>
+                      <h5 className="name">papi:125</h5>
+                      <h5 className="name">nant:49</h5>
+                    </div>
+           
+
+            </div>
+            </div>
+           
+           
+            
+            
+
+          </div>
+          
+
+        </section>
+          
+          
+      </div>
          </div>
     ) 
 }
-export default vueA
