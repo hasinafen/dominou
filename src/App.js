16df0041f3cdf82f8f3c66347eaf9a330b62a5e0
hasina <0000hasman@gmail.com>
2021-10-06 19:54:35 +0300

    vuec

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import './components/vueA.js';
 import './components/vueb.js';
-import './components/vueb.css'
+import './components/vueb.css';
+import './components/vuec.css';
 import './App.css';
 
 function App1 () {
 
 
     return(
-     <div>
-        <div className="vueb_container">
-        <section>
-          <div className="menu">
-          <i class="fa fa-home " ></i>
-            
-          </div>
-          <div className="titre">
-            <h1 className="h1">Nouvelle partie</h1>
-            <div className="type_de_joueur">
+    <div className="vuec_container">
+      <div className="menu">
 
-            <div className="input">
-              <h6 className="margin" >joueur1</h6>
-              <input src="text" className="test" placeholder="nom du joueur1"></input>
+      </div> 
+      <div className="title">
+        <h2 className="h2">partie de 3 joueur</h2>
+        <div className="score">
+        <h5 className="tojo">Tojo</h5>
+        <h6 className="total">total:45</h6>
+        <ul className="li">
 
-            </div>
-            <div className="input">
-              <h6 className="margin">joueur2</h6>
-              <input src="text" className="test" placeholder="nom du joueur2"></input>
-
-            </div>
-            <div className="input">
-              <h6 className="margin">joueur3</h6>
-              <input src="text" className="test" placeholder="nom du joueur3"></input>
-
-            </div>
-            <div className="input">
-              <h6 className="margin">Score gagnant</h6>
-              <input src="text" className="test" placeholder="score gagnant"></input>
-
-            </div>
-
-
-            </div>
-            <div className="click">
-              <button className="annuler">  <i class="fa fa-chevron-left"> </i> Annuler</button>
-              <button className="red"> Ajouter <i class="fa fa-plus"></i></button>
-            </div>
-          </div>
-        </section>
+          <li>10</li>
+          <li>5</li>
+          <li>30</li>
+        </ul>
+        <button className="ajouter"><i class="fa fa-plus"></i> ajouter score</button>
       </div>
-     </div>
+      <div className="score">
+        <h5 className="tojo">Papi</h5>
+        <h6 className="total">total:10</h6>
+        <ul className="li">
+
+          <li>5</li>
+          <li>5</li>
+          
+        </ul>
+        <button className="ajouter"><i class="fa fa-plus"></i> ajouter score</button>
+      </div>
+      <div className="score">
+        <h5 className="tojo">Nant</h5>
+        <div className="tota">
+        <h6>Aucun score</h6>
+        <h6 className="total">total:0</h6>
+       
+
+        </div>
+        <h6 className="inputs">Score</h6>
+        <input src="text" className=" marg" placeholder="nouveau score"></input>
+        <button className="ajout">sauver garder</button>
+      </div>
+      </div>
+      
+      
+
+    </div>
     )
  
 }
---
+++ src/components/vueb.js
@@ -0,0 +1,49 @@
+import './vueb.css';
+
+
+export const vueb =()=>{
+    return(
+        <div>
+        <div className="vueb_container">
+        <section className="vueb">
+          <div className="menu">
+          <i class="fa fa-home " ></i>
+            
+          </div>
+          <div className="titre">
+            <h1 className="h1">Nouvelle partie</h1>
+            <div className="type_de_joueur">
+
+            <div className="input">
+              <h6 className="margin" >joueur1</h6>
+              <input src="text" className="test" placeholder="nom du joueur1"></input>
+
+            </div>
+            <div className="input">
+              <h6 className="margin">joueur2</h6>
+              <input src="text" className="test" placeholder="nom du joueur2"></input>
+
+            </div>
+            <div className="input">
+              <h6 className="margin">joueur3</h6>
+              <input src="text" className="test" placeholder="nom du joueur3"></input>
+
+            </div>
+            <div className="input">
+              <h6 className="margin">Score gagnant</h6>
+              <input src="text" className="test" placeholder="score gagnant"></input>
+
+            </div>
+
+
+            </div>
+            <div className="click">
+              <button className="annuler">  <i class="fa fa-chevron-left"> </i> Annuler</button>
+              <button className="red"> Ajouter <i class="fa fa-plus"></i></button>
+            </div>
+          </div>
+        </section>
+      </div>
+     </div>
+    )
+}
