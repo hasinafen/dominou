b478bed2eb641319bac9e41f00039789369b31f7
hasina <0000hasman@gmail.com>
2021-10-10 21:08:17 +0300

    detail de page

--- src/App.js
+++ src/App.js
@@ -15,43 +15,60 @@ function App1 () {
       <div className="title">
         <h2 className="h2">partie de 3 joueur</h2>
         <div className="score">
-        <h5 className="tojo">Tojo</h5>
-        <h6 className="total">total:45</h6>
+        <h3 className="tojo">Tojo</h3>
+       
+        <div className="display">
         <ul className="li">
 
           <li>10</li>
           <li>5</li>
           <li>30</li>
         </ul>
-        <button className="ajouter"><i class="fa fa-plus"></i> ajouter score</button>
-      </div>
-      <div className="score">
-        <h5 className="tojo">Papi</h5>
-        <h6 className="total">total:10</h6>
-        <ul className="li">
-
-          <li>5</li>
-          <li>5</li>
-          
-        </ul>
-        <button className="ajouter"><i class="fa fa-plus"></i> ajouter score</button>
-      </div>
-      <div className="score">
-        <h5 className="tojo">Nant</h5>
-        <div className="tota">
-        <h6>Aucun score</h6>
-        <h6 className="total">total:0</h6>
+        <h5 className="total">Total : 45</h5>
        
+        </div>
+       
+        <button className="ajouter-1"><i class="fa fa-plus"></i> ajouter score</button>
+      </div>
+      <div className="score-1">
+        <h3 className="papi"> Papi</h3>
+        <div className="display-1">
+        <ul>
+          <li>5</li>
+          <li>4</li>
+
+        </ul>      
+        <h5 className="h5-1">Total:45 </h5>
+        </div>
+        <button className="ajouter-1"><i class="fa fa-plus"></i> ajouter score</button>
+
+        
+      </div>
+      <div className="score-2">
+        <h3 className="h3-1"> Nant</h3>
+        <div className="display-2"> 
+        <h5 className="h5-2"> Aucun score</h5>
+        {/* <h5 className="h5-3">Total: 0</h5> */}
+        <div className="display-3">
+        <h5 className="h3-5">total:</h5>
+        <h5 className="h3-6">120</h5>
 
         </div>
-        <h6 className="inputs">Score</h6>
-        <input src="text" className=" marg" placeholder="nouveau score"></input>
-        <button className="ajout">sauver garder</button>
+        </div>
+        <h6 className="h6-1">score</h6>
+        <input src="text" className="input-1" placeholder="Nouveau score"></input>
+        <button className="ajouter-2"><i class="fa fa-check"></i> ajouter score</button>
       </div>
-      </div>
-      
-      
+      <div className="score-3">
+        <div className="display-3">
+        <h3 className="h3-5">Score gagnant:</h3>
+        <h3 className="h3-6">120</h3>
 
+        </div>
+      </div>
+
+     
+      </div>
     </div>
     )
  
