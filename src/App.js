d8164f00823aab6742088a6544816694dcc8c899
hasina <0000hasman@gmail.com>
2021-10-13 13:09:34 +0300

    about-page

--- src/App.js
+++ src/App.js
@@ -1,94 +1,18 @@
-import './components/vueA.js';
-import './components/vueb.js';
-import './components/vueb.css';
-import './components/vuec.css';
+import './components/home.css';
+import './components/creation-page.js';
+import './components/creation-page';
+import './components/details-page.css';
+import './components/about-page.css';
+import './components/about-page.js';
 import './App.css';
 
 function App1 () {
 
 
     return(
-    <div className="vuec_container">
-      <div className="menu">
-      <button className="home"><i className="fa fa-home"></i></button>
-      
-      <button className="circle"><i className="fa fa-info-circle"></i></button>      
-      </div> 
-      <div className="title">
-      <div className="jone">
-        
-
-        </div>
-        
-        <div className="pen">
-        
-        <h2 className="h2">partie de 3 joueur <button className="button-1"><i class="fa fa-pen"></i></button></h2>
-
-        </div>
-        
-        <div className="score">
-        <h3 className="tojo">Tojo</h3>
-       
-        <div className="display">
-        <ul className="li">
-
-          <li>10</li>
-          <li>5</li>
-          <li>30</li>
-        </ul>
-        <h5 className="total">Total:</h5>
-        <h5 className="total-0">45</h5>
-       
-        </div>
-       
-        <button className="ajouter-1"><i class="fa fa-plus"></i> ajouter score</button>
-      </div>
-      <div className="score-1">
-        <h3 className="papi"> Papi</h3>
-        <div className="display-1">
-        <ul>
-          <li>5</li>
-          <li>4</li>
-
-        </ul>      
-        <h5 className="h5-1">Total: </h5>
-        <h5 className="h5-5">54</h5>
-        </div>
-        <button className="ajouter-3"><i class="fa fa-plus"></i> ajouter score</button>
-
+      <div>
         
       </div>
-      <div className="score-2">
-        <h3 className="h3-1"> Nant</h3>
-        <div className="display-2"> 
-        <h5 className="h5-2"> Aucun score</h5>
-        {/* <h5 className="h5-3">Total: 0</h5> */}
-        <div className="display-3">
-        <h5 className="h3-5">total:</h5>
-        <h5 className="h3-6">0</h5>
-
-        </div>
-        </div>
-        <h6 className="h6-1">score</h6>
-        <div className="float">
-        <input src="text" className="input-1" placeholder="Nouveau score" ></input>
-         <button className="times"><i class="fa fa-times" ></i> </button>
-        </div>
-        <button className="ajouter-2"><i class="fa fa-check"></i> ajouter score</button>
-      </div>
-      <div className="score-3">
-        <div className="display-3">
-        <h3 className="h3-8">Score gagnant:</h3>
-        <h3 className="h3-7">120</h3>
-
-        </div>
-      </div>
-
-     
-      </div>
-      
-     
-    </div>
     )
  
 }
---
+++ src/components/about-page.js
@@ -0,0 +1,52 @@
+import './about-page.css';
+
+ const about =()=>{
+     return(
+         <div>
+             <div className="about-container">
+        <div className="about-menu">
+          <button className="about-btn"><i className="fa fa-home"></i></button>
+          <button className="info"><i className="fa fa-info-circle"></i></button>
+        </div>
+        <div className="about-titre">
+          <h2 className="about-h2">A propos</h2>
+          <div className="class">
+         
+          </div>
+          <div className="diva">
+            <span> une application pour enregistre <br/>les scores de jeux dominos</span>
+            <span> une application pour enregistre <br/>les scores de jeux dominos</span>
+            <span> une application pour enregistre <br/>les scores de jeux dominos</span>
+             
+             <h3>Dévéloppé par:</h3>
+              <div className="about-fini">
+              <span className="feno">Feno Hasina</span>  
+             
+             <h1 className="rot"><i className="fa fa-user-circle"></i></h1>
+            <h4 className="about-logo"><i className='fa fa-globe-africa'></i></h4>
+             <h4 className="about-envelope"><i className="fa fa-envelope"></i></h4>    
+
+              </div>
+                   
+             
+             <div className="div">
+             <span className="fen">Nantenaina Fahendrena</span>  
+             
+             <h1 className="rote"><i className="fa fa-user-circle"></i></h1>
+              <h4 className="logo"><i className='fa fa-globe-africa'></i></h4>
+              <h4 className="envelope"><i className="fa fa-envelope"></i></h4>  
+             </div>
+          </div>
+         
+
+          
+          
+        </div>
+        
+        
+      </div>
+         </div>
+     )
+
+ }
+ export default about
